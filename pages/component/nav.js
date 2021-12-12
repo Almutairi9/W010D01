602d8a0db734c4f8aba5492d4a63e81063ae8173
Rawan Almutairi <rawan.fahad@tuwaiq.edu.sa>
2021-12-12 16:39:34 +0300

    done

---
+++ pages/component/nav.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+function Nav () {
+  return (
+    <ul>
+      <Link href="/">
+        <li> Home</li>
+      </Link>
+      <Link href="/about">
+        <li> About</li>
+      </Link>
+      <Link href="/contac">
+        <li> Contact</li>
+      </Link>
+      <Link href="/post">
+        <li> Post</li>
+      </Link>
+    </ul>
+  );
+};
+export default Nav;
